783e1c0e6894086e01d7b77e1dbcf9594e789b6e
Dave Erwin <daveerwin@Daves-MacBook-Pro.local>
2023-08-27 18:46:31 -0700

    Added: pokemons loading, grid css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PokemonDataService, Pokemondata } from './pokemon-data.service';
+import { Pokemon } from './pokemon-data.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'pokemon-angular';
+export class AppComponent implements OnInit {
+  
+  protected pokemonsdata: Pokemondata | undefined;
+  protected pokemon: any;
+  private page = 0;
+
+  constructor(
+    private pokemonDataService: PokemonDataService) {}
+
+
+  ngOnInit(): void {
+    this.getPokemons();
+    
+  }
+
+  getPokemons(): void {
+    this.pokemonDataService.getPokemons(this.page)
+    .subscribe(pokemonsdata => {
+      this.pokemonsdata = pokemonsdata;
+      
+      this.pokemonsdata.results.forEach((result, index) => {
+        console.log(result);
+        this.pokemonDataService.getPokemon(result.url).subscribe(pokemon => {
+          if (this.pokemonsdata) {
+            this.pokemonsdata.results[index].imageURL = pokemon.sprites.front_default;
+          }
+          
+        });
+      });
+      console.log("DERP", this.pokemonsdata)
+      //this.pokemonDataService.getPokemon(pokemonsdata.name);
+    });
+  }
+
 }
---
+++ src/app/pokemon-data.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PokemonDataService {
+
+  limit: number = 12;
+  pokemonsPaginatedURL = 'https://pokeapi.co/api/v2/pokemon?limit=12&offset=';
+  pokemonURL = 'https://pokeapi.co/api/v2/pokemon/';
+  
+
+  constructor(private http: HttpClient) { }
+
+  getPokemons(page: number): Observable<Pokemondata> {
+    return this.http.get<Pokemondata>(this.pokemonsPaginatedURL + (page * this.limit));
+  }
+
+  getPokemon(url: string): Observable<any> {
+    return this.http.get<any>(url);
+  }
+
+}
+
+export interface Pokemondata {
+  count: number;
+  next: string;
+  previous: string;
+  results: Pokemon[];
+}
+
+export interface Pokemon {
+  name: string;
+  url: string;
+  imageURL: string;
+}
